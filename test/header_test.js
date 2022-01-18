7dc3a23e9d9787043f22f9a0379826b2db5cf5ca
hyjoong <maplesosser@naver.com>
2022-01-18 17:30:18 +0900

    feat: e2e test 보류

--- test/header_test.js
+++ test/header_test.js
@@ -1,17 +1,17 @@
-Feature('Header');
+// Feature('Header');
 
-const pages = [
-  '/restaurants',
-  '/about',
-  '/not-exist',
-];
+// const pages = [
+//   '/restaurants',
+//   '/about',
+//   '/not-exist',
+// ];
 
-Scenario('헤더를 클릭하면 메인 페이지로 돌아옵니다.', ({ I }) => {
-  pages.forEach((page) => {
-    I.amOnPage(page);
+// Scenario('헤더를 클릭하면 메인 페이지로 돌아옵니다.', ({ I }) => {
+//   pages.forEach((page) => {
+//     I.amOnPage(page);
 
-    I.click('헤더');
+//     I.click('헤더');
 
-    I.see('Home');
-  });
-});
+//     I.see('Home');
+//   });
+// });
--- test/restaurants_test.js
+++ test/restaurants_test.js
@@ -1,31 +1,31 @@
-const assert = require('assert');
+// const assert = require('assert');
 
-Feature('Restaurant');
+// Feature('Restaurant');
 
-Scenario('카테고리와 지역을 모두 선택하면 레스토랑 목록을 볼 수 있습니다.', ({ I }) => {
-  I.amOnPage('/restaurants');
+// Scenario('카테고리와 지역을 모두 선택하면 레스토랑 목록을 볼 수 있습니다.', ({ I }) => {
+//   I.amOnPage('/restaurants');
 
-  I.click('서울');
-  I.click('한식');
+//   I.click('서울');
+//   I.click('한식');
 
-  I.see('양천주가');
-  I.see('한국식 초밥');
+//   I.see('양천주가');
+//   I.see('한국식 초밥');
 
-  I.click('부산');
-  I.see('밀면넘어져요');
-});
+//   I.click('부산');
+//   I.see('밀면넘어져요');
+// });
 
-Scenario('레스토랑 상세 페이지로 이동합니다.', async ({ I }) => {
-  I.amOnPage('/restaurants');
+// Scenario('레스토랑 상세 페이지로 이동합니다.', async ({ I }) => {
+//   I.amOnPage('/restaurants');
 
-  I.click('서울');
-  I.click('한식');
+//   I.click('서울');
+//   I.click('한식');
 
-  I.see('양천주가');
+//   I.see('양천주가');
 
-  I.click('양천주가');
+//   I.click('양천주가');
 
-  const url = await I.grabCurrentUrl();
+//   const url = await I.grabCurrentUrl();
 
-  assert.equal(/\/restaurants\/\d/.test(url), true);
-});
+//   assert.equal(/\/restaurants\/\d/.test(url), true);
+// });

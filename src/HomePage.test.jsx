e8409fc131767917e7cad9925e2f3d55792fe71d
hyjoong <maplesosser@naver.com>
2022-01-18 22:18:54 +0900

    test: homePage render test

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,16 +1,21 @@
 import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
+
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
+  const renderComponent = (path) => render(
+    <MemoryRouter initialEntries={[path]}>
+      <HomePage />
+    </MemoryRouter>,
+  );
+
   it('renders home page', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>,
-    );
+    const { container } = renderComponent('/');
 
     expect(container).toHaveTextContent('Home');
+    expect(container).toHaveTextContent('About');
+    expect(container).toHaveTextContent('Restaurants');
   });
 });
